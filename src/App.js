8568a1eb6f9ca3d4892118db61da217968f05df3
Andrew Durrant <andrew.durrant75@gmail.com>
2020-04-30 11:58:21 -0600

    utilize Link from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import cuid from 'cuid';
 
 import TodoContext from './TodoContext';
